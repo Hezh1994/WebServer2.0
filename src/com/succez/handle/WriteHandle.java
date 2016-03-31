16bc19ff4a6b2470f3384a3aed5f6a645b3363d6
Hezh1994 <hezh@succez.com>
2016-03-31 17:10:58 +0800

    抽取了关闭流、通道的方法。

--- src/com/succez/handle/WriteHandle.java
+++ src/com/succez/handle/WriteHandle.java
@@ -10,6 +10,12 @@ import java.nio.channels.SocketChannel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * 获取附加在key上的输入流，将请求的资源写入通道中。
+ * 
+ * @author succez
+ *
+ */
 public class WriteHandle implements Runnable {
 	private static final Logger LOG = LoggerFactory
 			.getLogger(WriteHandle.class);
@@ -20,6 +26,9 @@ public class WriteHandle implements Runnable {
 		this.key = key;
 	}
 
+	/**
+	 * 获取文件输入流得到文件通道，将通道的数据写入字节缓冲区中，将字节缓冲区中的内容写给客户端。
+	 */
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
@@ -36,21 +45,35 @@ public class WriteHandle implements Runnable {
 					try {
 						socketChannel.write(buffer);
 					} catch (IOException e) {
+						/**
+						 * 客户端取消下载后，则通道关闭，若继续向通道中执行写操作，则抛出I/O异常。
+						 */
 						LOG.info("客户端取消了下载");
-						is.close();
-						channel.close();
-						socketChannel.close();
+						closeAll(socketChannel, is, channel);
 						break;
 					}
 				}
 			} else {
-				is.close();
-				channel.close();
-				socketChannel.close();
+				closeAll(socketChannel, is, channel);
 			}
 		} catch (IOException e) {
 			LOG.error("写入信息失败：发生I/O错误");
 		}
 
+	}
+
+	/**
+	 * 关闭输入流、文件通道、客户端通道
+	 * 
+	 * @param socketChannel
+	 * @param is
+	 * @param channel
+	 * @throws IOException
+	 */
+	private void closeAll(SocketChannel socketChannel, FileInputStream is,
+			FileChannel channel) throws IOException {
+		is.close();
+		channel.close();
+		socketChannel.close();
 	}
 }
